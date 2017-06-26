ab77e566582f86ff9130e7302e65e91cf67ef084
Mario Steinhoff <steinhoff.mario@gmail.com>
2017-06-26 18:19:41 +0200

    feat(order): Add simple order model and pseudo test for demonstration

--- chaospizza/orders/models.py
+++ chaospizza/orders/models.py
@@ -1,5 +1,64 @@
 # pylint: disable=C0111
-# from django.db import models
+from django.db import models
 
 
-# Create your models here.
+ORDER_STATES = (
+    ('preparing', ''),
+    ('ordering', ''),
+    ('ordered', ''),
+    ('delivered', ''),
+    ('canceled', ''),
+)
+
+
+class Order(models.Model):
+    """
+    Represents an order which is used to help coordinate food delivery for multiple users.
+
+    Users can add order items as they like when the state is preparing.
+    """
+
+    coordinator = models.CharField(max_length=100)
+    restaurant_name = models.CharField(max_length=250)
+    state = models.CharField(max_length=16, choices=ORDER_STATES, default='preparing')
+    # TODO remove this and always generate from state changes?
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def add_item(self, participant, description, price, amount):
+        """Add a new item to this order."""
+        item = OrderItem(
+            order=self,
+            participant=participant,
+            description=description,
+            price=price,
+            amount=amount
+        )
+        item.save()
+
+
+class OrderItem(models.Model):
+    """
+    Order items within an order for individual users.
+
+    The same user may create multiple order items for different food.
+    """
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    participant = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    amount = models.PositiveIntegerField()
+
+
+class OrderStateChange(models.Model):
+    """
+    Created whenever the state of an Order instance has been changed.
+
+    Contains the old state, new state and an optional reason.
+    """
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    old_state = models.CharField(max_length=16, choices=ORDER_STATES)
+    new_state = models.CharField(max_length=16, choices=ORDER_STATES)
+    reason = models.CharField(max_length=1000, null=True)
--- chaospizza/orders/tests.py
+++ chaospizza/orders/tests.py
@@ -1,14 +1,27 @@
 # pylint: disable=C0111
 # pylint: disable=R0201
+from decimal import Decimal
 import pytest
+
 from django.contrib.auth.models import User
+from .models import Order, OrderItem
 
 
 @pytest.mark.django_db
 def test_my_user():
     """Test basic SQL query."""
-    try:
-        me_user = User.objects.get(username='me')
-        assert me_user.is_superuser
-    except User.DoesNotExist:
-        assert True
+    coordinator = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    user = User.objects.create_user('seth', 'seth@thebeatles.com', 'sethpassword')
+
+    order = Order(coordinator=coordinator, restaurant_name='Yolo')
+    order.save()
+    print(order.id)
+    print(order.coordinator)
+    assert order.state == 'preparing'
+
+    order.add_item(owner=user, description='hahaha', price=Decimal(7, 2), amount=1)
+
+    order_item = OrderItem.objects.filter(order=order).get()
+    print(order_item.description)
+
+    raise ValueError("yolo")
