0ac222ac0131867eb826e96a4f03b34e853167ea
Mario Steinhoff <steinhoff.mario@gmail.com>
2017-06-26 20:10:20 +0200

    feat(order): Add first test cases to test order model functionality
    Test cases:
    
    - New order has expected initial state
    - add_item wires up object relations as expected

---
+++ chaospizza/orders/tests/test_model.py
@@ -0,0 +1,39 @@
+# pylint: disable=C0111
+# pylint: disable=C0103
+# pylint: disable=R0201
+from decimal import Decimal
+import pytest
+
+from ..models import Order, OrderItem
+
+
+class TestOrderModel:
+    @pytest.fixture
+    def order(self):
+        """Return a new, empty Order instance, saved to DB."""
+        order = Order(coordinator='Bernd', restaurant_name='Yolo')
+        order.save()
+        return order
+
+    @pytest.mark.django_db
+    def test_new_order_has_preparing_state(self):  # noqa
+        order = Order(coordinator='Bernd', restaurant_name='Yolo')
+        order.save()
+        assert order.state == 'preparing'
+
+    @pytest.mark.django_db
+    def test_added_order_item_is_associated_with_order_and_user(self, order):  # noqa
+        order.add_item('Kevin', description='Test', price=Decimal('7.20'), amount=1)
+
+        order_item = OrderItem.objects.filter(order=order).get()
+        assert order_item.participant == 'Kevin'
+
+    @pytest.mark.django_db
+    def test_order_returns_all_items(self, order):  # noqa
+        order.add_item('Kevin', description='Test1', price=Decimal('7.21'), amount=1)
+        order.add_item('Kevin', description='Test2', price=Decimal('7.22'), amount=1)
+        order.add_item('Kevin', description='Test3', price=Decimal('7.23'), amount=1)
+        order.add_item('Kevin', description='Test4', price=Decimal('7.24'), amount=1)
+
+        number_of_items = OrderItem.objects.filter(order=order).count()
+        assert number_of_items == 4
