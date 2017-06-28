41e4008bb98f0973ecdff08193715b6467ba13d9
Mario Steinhoff <steinhoff.mario@gmail.com>
2017-06-28 02:39:36 +0200

    feat(order): Enforce only one order coordination per user at a time

--- chaospizza/orders/views.py
+++ chaospizza/orders/views.py
@@ -1,8 +1,11 @@
 # pylint: disable=C0111
 # pylint: disable=R0201
+from django.urls import reverse
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
+from django.contrib import messages
 
 from .models import Order
 
@@ -21,9 +24,17 @@ class CreateOrder(CreateView):
     fields = ['coordinator', 'restaurant_name']
     template_name_suffix = '_create'
 
+    def get(self, request, *args, **kwargs):
+        """Enforce only one active order per user at a time."""
+        if 'is_coordinator' in self.request.session and self.request.session['is_coordinator']:
+            messages.add_message(request, messages.INFO, 'You are already coordinating an order.')
+            return redirect(reverse('orders:list'))
+        return super(CreateOrder, self).get(request, *args, **kwargs)
+
     def form_valid(self, form):
         """Enable coordinator mode in session when data is valid."""
         response = super(CreateOrder, self).form_valid(form)
+        self.request.session['order_id'] = self.object.id
         self.request.session['name'] = self.object.coordinator
         self.request.session['is_coordinator'] = True
         return response
