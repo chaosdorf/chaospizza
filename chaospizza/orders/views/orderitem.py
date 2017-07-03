efcabbf0f84e1e91b13856fcbc98eb14e9f9d7c9
Mario Steinhoff <steinhoff.mario@gmail.com>
2017-07-03 05:05:13 +0200

    fix(order): Prevent modifying object items when order is preparing

--- chaospizza/orders/views/orderitem.py
+++ chaospizza/orders/views/orderitem.py
@@ -18,6 +18,17 @@ class CreateOrderItem(UserSessionMixin, CreateView):
     model = OrderItem
     fields = ['participant', 'description', 'price', 'amount']
 
+    def dispatch(self, request, *args, **kwargs):
+        """Ensure that the associated order's state is preparing."""
+        self.order = Order.objects.filter(pk=kwargs['order_slug']).get()
+        if not self.order.is_preparing:
+            messages.add_message(
+                request, messages.ERROR,
+                'Can not add order item, order is in state {}'.format(self.order.state)
+            )
+            return redirect('orders:view_order', order_slug=kwargs['order_slug'])
+        return super(CreateOrderItem, self).dispatch(request, *args, **kwargs)
+
     def get_initial(self):
         """Populate the participant name if the user is already known in the session."""
         return {'participant': self.username}
@@ -25,15 +36,19 @@ class CreateOrderItem(UserSessionMixin, CreateView):
     def get_context_data(self, **kwargs):
         """Load associated Order record."""
         context = super(CreateOrderItem, self).get_context_data(**kwargs)
-        context['order'] = Order.objects.filter(pk=self.kwargs['order_slug']).get()
+        context['order'] = self.order
         return context
 
     def form_valid(self, form):
         """Associate created OrderItem with existing Order and add the participant's name to the session state."""
-        order = Order.objects.filter(pk=self.kwargs['order_slug']).get()
         try:
             order_item = form.save(commit=False)
-            order.add_item(order_item.participant, order_item.description, order_item.price, order_item.amount)
+            self.order.add_item(
+                order_item.participant,
+                order_item.description,
+                order_item.price,
+                order_item.amount
+            )
             self.username = order_item.participant
         except ValueError as err:
             messages.add_message(self.request, messages.ERROR, 'Could not add order item: {}'.format(err))
@@ -50,7 +65,7 @@ class UpdateOrderItemForm(forms.ModelForm):
     participant = forms.CharField(disabled=True)
 
 
-class UpdateOrderItem(UpdateView):
+class UpdateOrderItem(UserSessionMixin, UpdateView):
     """Update a single order item."""
 
     model = OrderItem
@@ -58,13 +73,22 @@ class UpdateOrderItem(UpdateView):
     slug_url_kwarg = 'item_slug'
     form_class = UpdateOrderItemForm
 
-    # TODO check if order can be modified
     # TODO make sure user is allowed to edit
+    def dispatch(self, request, *args, **kwargs):
+        """Ensure that the associated order's state is preparing."""
+        self.order = Order.objects.filter(pk=kwargs['order_slug']).get()
+        if not self.order.is_preparing:
+            messages.add_message(
+                request, messages.ERROR,
+                'Can not edit order item, order is in state {}'.format(self.order.state)
+            )
+            return redirect(self.get_success_url())
+        return super(UpdateOrderItem, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         """Load associated Order record."""
         context = super(UpdateOrderItem, self).get_context_data(**kwargs)
-        context['order'] = Order.objects.filter(pk=self.kwargs['order_slug']).get()
+        context['order'] = self.order
         return context
 
     def get_success_url(self):
@@ -72,19 +96,29 @@ class UpdateOrderItem(UpdateView):
         return reverse('orders:view_order', kwargs={'order_slug': self.kwargs['order_slug']})
 
 
-class DeleteOrderItem(DeleteView):
+class DeleteOrderItem(UserSessionMixin, DeleteView):
     """Delete a single order item."""
 
     model = OrderItem
     slug_field = 'id'
     slug_url_kwarg = 'item_slug'
 
-    # TODO check if order can be modified
     # TODO make sure user is allowed to edit
+    def dispatch(self, request, *args, **kwargs):
+        """Ensure that the associated order's state is preparing."""
+        self.order = Order.objects.filter(pk=kwargs['order_slug']).get()
+        if not self.order.is_preparing:
+            messages.add_message(
+                request, messages.ERROR,
+                'Can not delete order item, order is in state {}'.format(self.order.state)
+            )
+            return redirect(self.get_success_url())
+        return super(DeleteOrderItem, self).dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         """Load associated Order record."""
         context = super(DeleteOrderItem, self).get_context_data(**kwargs)
-        context['order'] = Order.objects.filter(pk=self.kwargs['order_slug']).get()
+        context['order'] = self.order
         return context
 
     def get_success_url(self):
