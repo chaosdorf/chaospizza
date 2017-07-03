8e83e2e1cc1ad443275719fee1b18318a9df5e1b
Mario Steinhoff <steinhoff.mario@gmail.com>
2017-07-04 01:18:22 +0200

    feat(order): Add django admin for order model

--- chaospizza/orders/admin.py
+++ chaospizza/orders/admin.py
@@ -1,5 +1,22 @@
 # pylint: disable=C0111
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Order, OrderItem, OrderStateChange
 
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):  # noqa
+    model = OrderItem
+
+
+class OrderStateChangeInline(admin.TabularInline):  # noqa
+    model = OrderStateChange
+
+
+class OrderAdmin(admin.ModelAdmin):  # noqa
+    list_fields = ('coordinator', 'restaurant_name')
+    inlines = [
+        OrderItemInline,
+        OrderStateChangeInline,
+    ]
+
+
+admin.site.register(Order, OrderAdmin)
